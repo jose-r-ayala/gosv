67adc7b8fdcc2e1fa9a0f0898224960e363f227d
Kevin Grande <kevinbussines2020@gmail.com>
2024-11-12 00:26:23 -0600

    feat: 💄 Update UI language to Spanish

--- resources/js/Pages/RouteDetails.jsx
+++ resources/js/Pages/RouteDetails.jsx
@@ -3,6 +3,7 @@ import { Head, Link } from "@inertiajs/react";
 
 export default function RouteDetails({ ruta }) {
     console.log(ruta);
+
     const convertDate = (date) => {
         const currentDate = new Date(date);
         const dateParameters = {
@@ -20,6 +21,15 @@ export default function RouteDetails({ ruta }) {
         );
         return dateConverted;
     };
+
+    const howManyAvailableSeatsLeft = (availableSeats) => {
+        var seatsCounter = 0;
+        availableSeats.forEach((availableSeat) => {
+            seatsCounter += availableSeat.disponible;
+        });
+
+        return seatsCounter + " de " + availableSeats.length;
+    };
     return (
         <AuthenticatedLayout>
             <Head title="Route Details" />
@@ -33,50 +43,52 @@ export default function RouteDetails({ ruta }) {
                 </Link>
                 {/* Header */}
                 <div className="flex justify-between items-center mb-4">
-                    <h1 className="text-3xl font-bold">Route Details</h1>
+                    <h1 className="text-3xl font-bold">Detalle de ruta</h1>
                     <button
                         className="text-white px-4 py-2 rounded hover:bg-blue-700"
                         style={{ backgroundColor: "#3159BE" }}
                     >
-                        Book trip
+                        Reservar viaje
                     </button>
                 </div>
                 <div className="flex-1 border-b border-gray-300 mb-3"></div>
 
                 {/* Main Content */}
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-8 ">
                     {/* Description */}
                     <div className="space-y-4">
-                        <h2 className="font-semibold text-2xl">Description</h2>
+                        <h2 className="font-semibold text-2xl">Descripción</h2>
                         <div className="bg-gray-100 p-4 rounded shadow">
                             <div className="mb-2">
-                                <label className="block font-medium">
-                                    Starting Point
+                                <label className="block font-bold">
+                                    Punto de encuentro
                                 </label>
                                 <p>{ruta.direccion_encuentro}</p>
                             </div>
                             <div className="mb-2">
-                                <label className="block font-medium">
-                                    Destination Point
+                                <label className="block font-bold">
+                                    Punto de destino
                                 </label>
                                 <p>{ruta.direccion_destino}</p>
                             </div>
                             <div className="grid grid-cols-2 gap-4">
                                 <div>
-                                    <label className="block font-medium">
-                                        Available seats
+                                    <label className="block font-bold">
+                                        Asientos disponibles
                                     </label>
-                                    <p>{ruta.cupos[0].disponible}</p>
+                                    <p>
+                                        {howManyAvailableSeatsLeft(ruta.cupos)}
+                                    </p>
                                 </div>
                                 <div>
-                                    <label className="block font-medium">
-                                        Date
+                                    <label className="block font-bold">
+                                        Fecha
                                     </label>
                                     <p>{convertDate(ruta.fecha_hora_salida)}</p>
                                 </div>
                                 <div>
-                                    <label className="block font-medium">
-                                        Price
+                                    <label className="block font-bold">
+                                        Precio
                                     </label>
                                     <p>$ {ruta.precio}</p>
                                 </div>
@@ -85,9 +97,9 @@ export default function RouteDetails({ ruta }) {
                     </div>
 
                     {/* Map */}
-                    <div className="w-full h-64 bg-gray-200 rounded shadow">
+                    <div className="space-y-4 bg-gray-100 rounded shadow flex items-center justify-center">   
                         {/* Placeholder for map */}
-                        <p className="text-center pt-28">Map View</p>
+                        <p className="text-center font-bold">Map View</p>
                     </div>
                 </div>
 
@@ -96,7 +108,7 @@ export default function RouteDetails({ ruta }) {
                     {/* Driver Info */}
                     <div className="h-[200px] bg-gray-100 rounded shadow overflow-auto">
                         <h2 className="font-semibold text-2xl mb-4 p-4">
-                            Driver
+                            Conductor
                         </h2>
                         <div className="flex items-center justify-between space-x-4 p-4">
                             <div className="flex gap-3">
@@ -119,7 +131,7 @@ export default function RouteDetails({ ruta }) {
                                 className="ml-auto text-white px-5 py-2 rounded hover:bg-green-600 text-sm"
                                 style={{ backgroundColor: "#158E5A" }}
                             >
-                                Send a message
+                                Enviar mensaje
                             </button>
                         </div>
                     </div>
@@ -127,7 +139,7 @@ export default function RouteDetails({ ruta }) {
                     {/* Additional Notes */}
                     <div className="h-[200px] bg-gray-100 rounded shadow overflow-auto">
                         <h2 className="font-semibold text-2xl mb-4 p-4">
-                            Additional Notes
+                            Notas
                         </h2>
                         <div className="p-4"></div>
                     </div>
@@ -135,7 +147,7 @@ export default function RouteDetails({ ruta }) {
 
                 {/* Comments Section */}
                 <div className="mt-8">
-                    <h2 className="font-semibold text-2xl mb-4">Comments</h2>
+                    <h2 className="font-semibold text-2xl mb-4">Comentarios</h2>
                     <div className="bg-gray-100 p-4 rounded shadow h-[200px] overflow-auto">
                         {ruta.comentarios.map(
                             ({ contenido, fecha, user, id }) => {
